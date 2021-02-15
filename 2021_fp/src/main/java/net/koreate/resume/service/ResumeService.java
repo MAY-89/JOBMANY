50c12379240bec2c5cfa4c121ff72580bdb77eb7
KTE-20 <KTE-20@DESKTOP-ULEPM8H>
2021-02-15 13:53:39 +0900

    now

--- 2021_fp/src/main/java/net/koreate/resume/service/ResumeService.java
+++ 2021_fp/src/main/java/net/koreate/resume/service/ResumeService.java
@@ -1,34 +1,69 @@
 package net.koreate.resume.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import net.koreate.resume.dao.ResumeDAO;
 import net.koreate.resume.vo.ResumeVO;
 import net.koreate.util.PageMaker;
 import net.koreate.util.SearchCriteria;
 
-public interface ResumeService {
+@Service
+public class ResumeServiceImpl {
 	
-	//전체 이력서
-	public List<ResumeVO> list(SearchCriteria cri) throws Exception;
+	@Inject
+	ResumeDAO dao;
 	
-	//좋아요순 이력서
-	public List<ResumeVO> likeList(SearchCriteria cri) throws Exception;
+	public Map<String,Object> list(SearchCriteria cri) throws Exception {
+		System.out.println("전체 리스트 불러오기");
+		Map<String,Object> map = new HashMap<String, Object>();
+		map.put("list", dao.resumeList(cri));
+		map.put("pm", getPageMaker(cri));
+		map.put("likeList", dao.resumeLikeList());
+		return map;
+	}
 	
-	//페이지 블록 정보
-	 public PageMaker getPageMaker(SearchCriteria cri) throws Exception;
+	public PageMaker getPageMaker(SearchCriteria cri) throws Exception {
+		int totalcnt = dao.getTotalList();
+		return new PageMaker(cri, totalcnt);
+	}
+
+	public void insert(ResumeVO vo) throws Exception {
+		System.out.println("이력서 정보 : "+vo);
+		dao.writeResume(vo);
+		
+	}
+
+	public ResumeVO select(int rno) throws Exception {
+		System.out.println("이력서 하나 불러오기 : "+dao.selectOneResume(rno));
+		return dao.selectOneResume(rno);
+	}
 	
-	//이력서 쓰기
-	public void insert(ResumeVO vo) throws Exception;
+	public void updateViews(int rno)throws Exception{
+		dao.views(rno);
+	}
 	
-	//이력서 읽기
-	public ResumeVO select(int rno) throws Exception;
-	
-	//이력서 수정
-	public ResumeVO update(ResumeVO vo) throws Exception;
-	 
-	//이력서 삭제
-	public void showHide(int rno) throws Exception;
-	
-	//좋아요 클릭
-	public void like(int rno) throws Exception;
+	public void update(ResumeVO vo) throws Exception {
+		System.out.println("이력서 수정");
+		dao.updateResume(vo);
+	}
+
+	public void showHide(int rno) throws Exception {
+		System.out.println("이력서비공개");
+		if(dao.selectOneResume(rno).getShowhide().equals("y")) {
+			dao.hideResume(rno);	
+		}else {
+			dao.showResume(rno);
+		}
+	}
+
+	public void like(int rno) throws Exception {
+		dao.likeResume(rno);
+	}
+
 }
